5f2f96d158d267eb9a107f67ae57efe5d744535d
Clara Alejos <calejs00@estudiantes.unileon.es>
2024-06-08 00:38:22 +0200

    g

---
+++ src/vista/ui/PruebaScroll.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'PruebaScrollxWvHXJ.ui'
+##
+## Created by: Qt User Interface Compiler version 5.15.2
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        if not MainWindow.objectName():
+            MainWindow.setObjectName(u"MainWindow")
+        MainWindow.resize(800, 600)
+        self.centralwidget = QWidget(MainWindow)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.Titulo = QLabel(self.centralwidget)
+        self.Titulo.setObjectName(u"Titulo")
+        self.Titulo.setGeometry(QRect(300, 20, 231, 91))
+        font = QFont()
+        font.setFamily(u"Bell MT")
+        font.setPointSize(24)
+        font.setBold(False)
+        font.setWeight(50)
+        self.Titulo.setFont(font)
+        self.widget = QWidget(self.centralwidget)
+        self.widget.setObjectName(u"widget")
+        self.widget.setGeometry(QRect(570, 210, 16, 161))
+        self.scrollBar = QScrollBar(self.centralwidget)
+        self.scrollBar.setObjectName(u"scrollBar")
+        self.scrollBar.setGeometry(QRect(600, 120, 21, 351))
+        self.scrollBar.setOrientation(Qt.Vertical)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QMenuBar(MainWindow)
+        self.menubar.setObjectName(u"menubar")
+        self.menubar.setGeometry(QRect(0, 0, 800, 26))
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QStatusBar(MainWindow)
+        self.statusbar.setObjectName(u"statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+
+        QMetaObject.connectSlotsByName(MainWindow)
+    # setupUi
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        self.Titulo.setText(QCoreApplication.translate("MainWindow", u"Lista de Objetos", None))
+    # retranslateUi
+
